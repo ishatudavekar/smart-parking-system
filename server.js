2a1dfece965fe5e4809e39e6d7911fba9ad59128
vjaythombre <vjaythombre@gmail.com>
2026-05-05 14:31:28 +0530

    Fix static pathing and add animated background cars

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 // Setup Middleware
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
